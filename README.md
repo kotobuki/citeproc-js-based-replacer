596d3b44f427134c9662903f424d46c156ba13dd
Shigeru Kobayashi <kotobuki@users.noreply.github.com>
2023-04-15 14:03:33 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# citeproc-js-based-replacer
+# `citeproc-js-based-replacer`
+
+## About this tool
+
+`citeproc-js-based-replacer` is a tool for generating [CSL-M](https://citeproc-js.readthedocs.io/en/latest/csl-m/index.html) compatible citations using the [`citeproc-js`](https://github.com/Juris-M/citeproc-js) library. This tool replaces citation keys (`@key`) in Markdown files and adds a bibliography if a header with the text "Bibliography" or "参考文献" is present.
+
+## Features
+
+- Replaces citation keys (`@key`) in Markdown files
+- Adds a bibliography if a "Bibliography" or "参考文献" header is present
+- Specifies bibliography file, CSL file, and uncited items to include in the bibliography in the YAML header of the Markdown file
+
+## Preparation
+
+### Steps
+
+1. Run `npm install` to install the required libraries
+2. Obtain the desired CSL-M file from sources like [jm-styles](https://github.com/Juris-M/jm-styles)
+3. Obtain the necessary locale file(s) (e.g., `locales-en-US.xml`) from [citation-style-language / locales](https://github.com/citation-style-language/locales/tree/6b0cb4689127a69852f48608b6d1a879900f418b)
+4. Generate a bibliography with [Jurism](https://juris-m.github.io/) or [Zotero](https://www.zotero.org/)
+
+### Example file layout
+
+- `citeproc-js-based-replacer.js`
+- `jm-japan-sociological-society.csl`
+- `locales-en-US.xml`
+- `locales-ja-JP.xml`
+- `bibliography.json`
+- `input.md`
+- `node_modules/`
+
+## Usage
+
+```shell
+pandoc input.md -s --filter=./citeproc-js-based-replacer.js -o output.md
+```
+
+## Development
+
+This project is in its early stages, and we welcome feedback and bug reports. Pull requests are highly appreciated!
+
+---
+
+## 本ツールについて
+
+`citeproc-js-based-replacer`は、[`citeproc-js`](https://github.com/Juris-M/citeproc-js)ライブラリを使用して[CSL-M](https://citeproc-js.readthedocs.io/en/latest/csl-m/index.html)対応の文献引用を生成するためのツールです。このツールは、Markdownファイル内の引用キー（`@key`）を置き換え、"参考文献"または"Bibliography"というヘッダーがある場合に文献目録を追加します。
+
+## 機能
+
+- Markdownファイル内の引用キー（`@key`）を置き換えます
+- "参考文献"または"Bibliography"というヘッダーがある場合に文献目録を追加します
+- MarkdownファイルのYAMLヘッダー部分で、bibliographyファイル、CSLファイル、引用しないが参考文献に含めたい文献の指定を行います
+
+## 準備
+
+### 手順
+
+1. `npm install`を実行して必要なライブラリをインストールする
+2. [jm-styles](https://github.com/Juris-M/jm-styles) などから、使用したいCSL-Mファイルを入手する
+3. [citation-style-language / locales](https://github.com/citation-style-language/locales/tree/6b0cb4689127a69852f48608b6d1a879900f418b) から必要なlocaleファイル（例：`locales-en-US.xml`）を入手する
+4. [Jurism](https://juris-m.github.io/)や[Zotero](https://www.zotero.org/)によりJSON形式のbibliographyを生成する
+
+### ファイルの配置例
+
+- `citeproc-js-based-replacer.js`
+- `jm-japan-sociological-society.csl`
+- `locales-en-US.xml`
+- `locales-ja-JP.xml`
+- `bibliography.json`
+- `input.md`
+- `node_modules/`
+
+## 使い方
+
+```shell
+pandoc input.md -s --filter=./citeproc-js-based-replacer.js -o output.md
+```
+
+## 開発
+
+このプロジェクトはまだ開発が始まったばかりであり、要望や不具合に関するレポートを歓迎します。とくに、プルリクエストは大歓迎です！
---
+++ citeproc-js-based-replacer.js
@@ -0,0 +1,225 @@
+const fs = require("fs");
+const path = require("path");
+const citeproc = require("citeproc-js-node");
+
+const debugMode = false;
+
+/**
+ * Outputs debug log messages when debugMode is true.
+ * debugModeがtrueの場合、デバッグログメッセージを出力します。
+ *
+ * @function debugLog
+ * @param {...any} args - Arguments to pass to console.log
+ */
+function debugLog(...args) {
+  if (debugMode) {
+    console.log(...args);
+  }
+}
+
+/**
+ * Collects citation keys and citation objects from the input object.
+ * 入力オブジェクトから引用キーと引用オブジェクトを収集します。
+ *
+ * @function collectCitations
+ * @param {any} obj - The input object to scan for citations
+ * @returns {any} The input object with citations collected
+ */
+function collectCitations(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(collectCitations);
+  } else if (typeof obj === "object" && obj !== null) {
+    if (obj.t === "Cite") {
+      const citationId = obj.c[0][0].citationId;
+      const noteIndex = citationKeys.length;
+      citationKeys.push(citationId);
+      citationObjects.push({
+        citationID: `${citationId}_${noteIndex}`,
+        citationItems: [{ id: citationId }],
+        properties: { noteIndex: noteIndex },
+      });
+
+      return;
+    } else {
+      return Object.fromEntries(
+        Object.entries(obj).map(([key, value]) => [
+          key,
+          collectCitations(value),
+        ])
+      );
+    }
+  } else {
+    return;
+  }
+}
+
+/**
+ * Replaces citations in the input object with their formatted text.
+ * 入力オブジェクト内の引用を、整形されたテキストに置き換えます。
+ *
+ * @function replaceCitations
+ * @param {any} obj - The input object containing citations to replace
+ * @returns {any} The input object with citations replaced
+ */
+function replaceCitations(obj) {
+  if (Array.isArray(obj)) {
+    return obj.map(replaceCitations);
+  } else if (typeof obj === "object" && obj !== null) {
+    if (obj.t === "Cite") {
+      return {
+        t: "Str",
+        c: formattedCitations.shift(),
+      };
+    } else {
+      return Object.fromEntries(
+        Object.entries(obj).map(([key, value]) => [
+          key,
+          replaceCitations(value),
+        ])
+      );
+    }
+  } else {
+    return obj;
+  }
+}
+
+/**
+ * Converts the bibliography result from citeproc-js to a Pandoc-formatted object.
+ * citeproc-jsからの参考文献結果をPandoc形式のオブジェクトに変換します。
+ *
+ * @function convertBibResultToPandoc
+ * @param {Array} bibResult - The bibliography result from citeproc-js
+ * @returns {Array} A Pandoc-formatted object representing the bibliography
+ */
+function convertBibResultToPandoc(bibResult) {
+  return bibResult[1].map((bibEntry) => {
+    const markdownBibEntry = bibEntry
+      .trim()
+      .replace(/<i>/g, "*")
+      .replace(/<\/i>/g, "*")
+      .replace(/<div class="csl-entry">/g, "")
+      .replace(/<\/div>/g, "")
+      .replace(/&ndash;/g, "--")
+      .replace(/&mdash;/g, "---")
+      .replace(/&amp;/g, "&")
+      .replace(/&lt;/g, "<")
+      .replace(/&gt;/g, ">");
+
+    return {
+      t: "Para",
+      c: [
+        {
+          t: "RawInline",
+          c: ["markdown", markdownBibEntry],
+        },
+      ],
+    };
+  });
+}
+
+process.stdin.setEncoding("utf8");
+
+let inputData = "";
+
+process.stdin.on("data", (chunk) => {
+  inputData += chunk;
+});
+
+const citationKeys = [];
+const citationObjects = [];
+let citeprocEngine = undefined;
+
+let formattedCitations = undefined;
+
+process.stdin.on("end", () => {
+  let dataObj = JSON.parse(inputData);
+
+  const languageCodes = ["en-US", "ja-JP"];
+  let locales = {};
+
+  languageCodes.forEach((languageCode) => {
+    const localeFilePath = path.join(__dirname, `locales-${languageCode}.xml`);
+    try {
+      const data = fs.readFileSync(localeFilePath, "utf8");
+      locales[languageCode] = data;
+    } catch (err) {
+      console.error(`Error reading locale file: ${localeFilePath}`, err);
+    }
+  });
+
+  const cslFile = dataObj.meta.csl.c[0].c;
+  debugLog(`cslFile: ${cslFile}`);
+  const style = fs.readFileSync(cslFile, "utf-8");
+
+  const bibliographyFile = dataObj.meta.bibliography.c[0].c;
+  debugLog(`bibliographyFile: ${bibliographyFile}`);
+  const bibliography = JSON.parse(fs.readFileSync(bibliographyFile));
+
+  const sys = {
+    retrieveItem: function (itemID) {
+      return bibliography.find((entry) => entry.id === itemID);
+    },
+    retrieveLocale: function (lang) {
+      return locales[lang];
+    },
+  };
+
+  citeprocEngine = new citeproc.CSL.Engine(sys, style);
+  citeprocEngine.setOutputFormat("text");
+
+  const citableItemIds = bibliography.map((item) => item.id);
+  debugLog(`citableItemIds: ${JSON.stringify(citableItemIds)}`);
+  citeprocEngine.updateItems(citableItemIds);
+
+  const uncitedItemIds = dataObj.meta.nocite
+    ? dataObj.meta.nocite.c.map((para) => para.c[0].c[0][0].citationId)
+    : [];
+  debugLog(`uncitedItemIds: ${JSON.stringify(uncitedItemIds)}`);
+
+  if (uncitedItemIds.length > 0) {
+    citeprocEngine.updateUncitedItems(uncitedItemIds);
+  }
+
+  collectCitations(dataObj.blocks);
+
+  formattedCitations = citationObjects.map((citation, index) => {
+    const predecessor = citationObjects
+      .slice(0, index)
+      .map((prevCitation) => [
+        prevCitation.citationID,
+        prevCitation.properties.noteIndex,
+      ]);
+    const successor = [];
+    const result = citeprocEngine.processCitationCluster(
+      citation,
+      predecessor,
+      successor
+    );
+    if (result[0].bibchange === false) {
+      return result[1][1] && result[1][1][1]
+        ? result[1][1][1]
+        : result[1][0][1];
+    }
+
+    return result[1][0][1];
+  });
+
+  dataObj.blocks = replaceCitations(dataObj.blocks);
+
+  const bibliographyHeaderIndex = dataObj.blocks.findIndex(
+    (block) =>
+      block.t === "Header" &&
+      (block.c[1][0] === "参考文献" || block.c[1][0] === "Bibliography")
+  );
+
+  if (bibliographyHeaderIndex >= 0) {
+    citeprocEngine.setOutputFormat("html");
+    const bibResult = citeprocEngine.makeBibliography();
+    const pandocBibResult = convertBibResultToPandoc(bibResult);
+    dataObj.blocks.splice(bibliographyHeaderIndex + 1, 0, ...pandocBibResult);
+  }
+
+  process.stdout.write(JSON.stringify(dataObj));
+});
+
+process.stdin.resume();
